51c356125ece1f0356b42008a515e61febc7c472
Sady Luaxy <eduardosmithpitxo@gmail.com>
2019-10-09 02:24:18 +0100

    Fizalizei O Login Do Site E Criei A Area De Cadastro De Administrador Demo

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-
+	session_start();
 	require_once("vendor/autoload.php");
 
 	use \Slim\slim;
 	use \Seguradora\Page;
 	use \Seguradora\PageAdmin;
+	use \Seguradora\Model\User;
+	
 
 	$app = new \Slim\Slim();
 
@@ -20,12 +22,38 @@
 
 	$app->get('/admin', function() {
 
+		User::verifyLogin();
 		$page = new PageAdmin();
 
 		$page->setTpl("index");
 
 	});
 
+	$app->get('/admin/login', function(){
+
+		$page = new PageAdmin();
+		$page->setTpl("user-login");
+
+	});
+
+	$app->post('/admin/login', function(){
+
+		User::login($_POST["user"], $_POST["senha"]);
+			header("Location: /admin");
+		exit;
+		
+	});
+
+	$app->get('/admin/logout', function(){
+
+		User::logout();
+
+		header("/admin/login");
+		exit;
+
+	});
+
 	$app->run();
+
 
 ?>
